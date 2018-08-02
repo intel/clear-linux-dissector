06884868a83b080949622d57d7124d5471a94bcc
Rusty Lynch <rusty.lynch@intel.com>
2018-08-02 09:40:57 -0700

    Updated bundles2packages to resolve all runtime deps on each of the packages listed for each one of the input bundles

--- cmd/bundles2packges/main.go
+++ cmd/bundles2packges/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"archive/tar"
-	"compress/gzip"
 	"bufio"
+	"compress/gzip"
 	"flag"
 	"fmt"
+	"github.com/awalterschulze/gographviz"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 	"net/http"
-	"regexp"
 )
 
 func name_from_header(header string, version int) string {
@@ -23,20 +25,68 @@ func name_from_header(header string, version int) string {
 	return match[len(match) - 1]
 }
 
+func dump_package_deps(g *gographviz.Graph, p string, visited map[string]bool) []string {
+	var ret []string;
+	visited[p] = true
+	for edge_name, edge := range g.Edges.SrcToDsts {
+		if edge_name == p {
+			for destination, _ := range edge {
+				for relation_name, relation := range g.Relations.ParentToChildren {
+					if destination == relation_name {
+						for dname, _ := range relation {
+							if !visited[dname] {
+								for _, pname := range dump_package_deps(g, dname, visited) {
+									ret = append(ret, pname)
+								}
+							}
+							ret = append(ret, dname)
+						}
+					}
+				}
+			}
+		}
+	}
+	return ret
+}
+
 func main() {
 	var clear_version int
-	flag.IntVar(&clear_version, "v", -1, "Clear Linux version")
+	flag.IntVar(&clear_version, "clear_version", -1, "Clear Linux version")
 
 	var base_url string
-	flag.StringVar(&base_url, "u",
+	flag.StringVar(&base_url, "url",
 		"https://github.com/clearlinux/clr-bundles",
 		"Base URL for downloading release archives of clr-bundles")
+
+	var graph_filename string
+	flag.StringVar(&graph_filename, "dependency_graph", "",
+		"Input dependency graph file")
 	
 	flag.Usage = func() {
 		fmt.Printf("USAGE for %s\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+
+	args := flag.Args()
+
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatal()
+	}
+	if info.Mode() & os.ModeNamedPipe != 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			new_args := strings.Split(scanner.Text(), " ")
+			args = append(args, new_args...)
+		}
+	}
+
+	if (graph_filename == "") {
+		fmt.Println("No dependency graph file provided")
+		flag.Usage()
+		os.Exit(-1)
+	}
 
 	if (clear_version == -1) {
 		f, err := os.Open("/usr/lib/os-release")
@@ -119,18 +169,43 @@ func main() {
 
 	}
 
-	res := make(map[string]bool)
-	for _, target_bundle := range flag.Args() {
+	pkgs_before_deps := make(map[string]bool)
+	for _, target_bundle := range args {
 		for _, p := range bundles[target_bundle] {
-			res[p] = true
+			pkgs_before_deps[p] = true
 		}
 		for _, b := range deps[target_bundle] {
 			for _, p := range bundles[b] {
-				res[p] = true
+				pkgs_before_deps[p] = true
 			}			
 		}
 	}
-	for p, _ := range res {
-	 	fmt.Println(p)
+
+	f, err := os.Open(graph_filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+	content, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graph, err := gographviz.Read(content)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	visited := make(map[string]bool)
+	for pkg, _ := range pkgs_before_deps {
+		pkg = fmt.Sprintf("\"%s\"", pkg)
+		if !visited[pkg] {
+			dump_package_deps(graph, pkg, visited)
+		}
+	}
+	for k, _ := range visited {
+		fmt.Println(strings.Replace(k, "\"", "", 2))
 	}
 }
